9953734a70e1654f82b6566ffcac552d182e606e
izharsha07 <80449371+izharsha07@users.noreply.github.com>
2021-04-02 16:38:23 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    //CHANGE ALL OF THESE
+    channelid: "826719764905721856",
+    token: "ODI3MDgwNTg3ODIxOTczNTQ0.YGV00g.hh9c5eiiRNiynaLp59mZkWGChiA",
+    botname: "Ryuunosuke",
+
+    // API AUTHORIZATION
+    authorization: "ODE5MjAzOTE3ODc1MzgwMjY0.MTYxNzM2MDkxNjI3OQ==.f2a366e9f6f8f2c0e96444f878677330"
+}
